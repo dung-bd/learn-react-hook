492c23280d4d3c4436a358e3e13e8ca937afe2dc
dung <dungbachdang123@gmail.com>
2021-05-14 21:33:10 +0700

    aux

---
+++ src/Components/index.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react"
+//import PropTypes from "prop-types"
+
+/*ColorBox.propTypes={
+
+};*/
+function getRandomColor(){
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const randomIndex = Math.trunc(Math.random() * 5)
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox(){
+    /*const initColor =localStorage.getItem('box-color') || 'deeppink'
+      const[color, setColor] = useState(initColor)*/
+      const[color, setColor] = useState(()=>{
+        const initColor =localStorage.getItem('box-color') || 'deeppink'
+        return initColor
+      })
+      function handleBoxClick(){
+          const newColor = getRandomColor();
+          setColor(newColor)
+          localStorage.setItem('box-color', newColor)
+      }
+    return(
+        <div className="color-box" style={{backgroundColor: color}} onClick={handleBoxClick}>
+            COLOR BOX
+        </div>
+    )
+}
+export default ColorBox
